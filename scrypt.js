96b61ec04134432d724637f6d1b56dc3de6ee3fb
AlexandreSilveiraLucianete <alexandre.sl@outlook.com>
2025-04-19 15:58:57 -0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,4 @@
+document.getElementById('contato-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // impede o envio real do formulário
+    alert('Obrigado! Suas informações foram registradas.');
+});
